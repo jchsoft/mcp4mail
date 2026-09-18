50db186be9fa1d51d5e8f6c70776332d50f58171
josefchmel <chmel@jchsoft.cz>
2026-09-18 08:04:05 +0200

    Add branded favicon and Open Graph/Twitter image
    The site was using the default Rails placeholder icon (plain red
    circle) and had no Open Graph or Twitter Card meta tags, so shared
    links showed no icon or preview image in chat apps and social media.
    
    - Replace icon.svg/icon.png with a branded blue envelope mark, add
      apple-touch-icon.png and favicon.ico
    - Add a 1200x630 og-image.png and wire up og:*/twitter:* meta tags
      plus a meta description in both layouts

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -7,4 +7,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h1", "mcp4mail"
   end
+
+  test "home page has favicon links and Open Graph / Twitter meta tags" do
+    get root_url
+
+    assert_response :success
+    assert_select "link[rel=icon][href='/favicon.ico']"
+    assert_select "link[rel=icon][href='/icon.svg'][type='image/svg+xml']"
+    assert_select "link[rel='apple-touch-icon'][href='/apple-touch-icon.png']"
+
+    assert_select "meta[property='og:title']"
+    assert_select "meta[property='og:description']"
+    assert_select "meta[property='og:image'][content=?]", "#{root_url}og-image.png"
+    assert_select "meta[name='twitter:card'][content='summary_large_image']"
+    assert_select "meta[name='twitter:image'][content=?]", "#{root_url}og-image.png"
+  end
 end
